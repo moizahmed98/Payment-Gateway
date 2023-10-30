2117d2840411e67c27310bdbe8364ff8063d3ade
ZABEEHZEE <zabeeh0731@gmail.com>
2023-10-29 22:52:38 -0700

    New Org Zabeeh

--- force-app/main/default/lwc/admin_Panel_Main/admin_Panel_Main.js
+++ force-app/main/default/lwc/admin_Panel_Main/admin_Panel_Main.js
@@ -1,4 +1,4 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
 import StripeAdminAuth from '@salesforce/apex/AdminAuthenticationController.StripeAdminAuth';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
@@ -202,14 +202,94 @@ console.log(this.publishKey);
 }
 
 @track resultresponse;
+response;
+error;
 callApexMethod() {
-    StripeAdminAuth({ 
+   
+
+    console.log('ftn is called');
+    
+
+    StripeAdminAuth({  
+        stripeMerchantName: this.merchantName,
+        stripeSecretApiKey: this.secretKey,
+        stripePublishableApiKey: this.publishKey })
+    .then(result => {
+        // Handle the successful response
+        this.response = JSON.parse(result);
+        this.error = null;
+        console.log('RESULT  : '+JSON.parse(JSON.parse(result)));
+        console.log('RESULT 2  : '+JSON.stringify(this.response));
+        console.log('RESULT  3 : '+this.response);
+        
+
+       
+        //////////////////////**********************************HAndel Return**************************************/////////////////////////////////////////////////////////
+        
+        if(this.response.settings.dashboard.display_name != null  )
+        {
+                        console.log('error is  null  and the display name is '+this.response.settings.dashboard.display_name);
+                        console.log('this is the input merchant name : '+this.merchantName);
+
+                        if(this.response.settings.dashboard.display_name === this.merchantName)
+                        {
+                            console.log('the  display name is correct and the merchant is authenticated');
+                            const evt = new ShowToastEvent({
+                                title: 'Success',
+                                message: 'Authentication successful',
+                                variant: 'success',
+                            });
+                            this.dispatchEvent(evt);
+                        
+                        }
+                        else if(this.response.settings.dashboard.display_name != this.merchantName)
+                        {
+                            console.log('display name is not correct : '+this.merchantName);
+                            const evt = new ShowToastEvent({
+                                title: 'Toast Error',
+                                message: 'Merchant Name is Incorrect',
+                                variant: 'error',
+                                mode: 'dismissable'
+                            });
+                            this.dispatchEvent(evt);
+
+                        }
+                    
+        }
+        // Handle the case where the authentication with Stripe resulted in an error
+        else if(this.response.error.message != null   )
+        {
+            
+            
+            console.log('error message  : '+this.response.error.message);
+            const evt = new ShowToastEvent({
+                title: 'Toast Error',
+                message: this.response.error.message,
+                variant: 'error',
+                mode: 'dismissable'
+            });
+            this.dispatchEvent(evt);
+            
+
+        }
+        console.log('final response that is retuned  : ');
+       
+    })
+    .catch(error => {
+        // Handle any errors
+        this.response = null;
+        this.error = error;
+    });
+
+    
+
+    /*StripeAdminAuth({ 
         stripeMerchantName: this.merchantName,
         stripeSecretApiKey: this.secretKey,
         stripePublishableApiKey: this.publishKey
             })
                 .then(result => {
-                    console.log('this is result'+result[0]);
+                    console.log('this is result'+result);
                     this.resultresponse = result;
                     
                     console.log('this is result stringify :'+ JSON.stringify(this.resultresponse));
@@ -229,6 +309,6 @@ callApexMethod() {
                 .catch(error => {
                     // Handle errors
                     console.error(error);
-                });
+                });*/
         }
 }
